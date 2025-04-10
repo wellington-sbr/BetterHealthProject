99308c8b8ae8a3eaede001571ab3e34cdd70dbea
HP <krisoc123@gmail.com>
2025-04-10 14:35:08 +0200

    Add user authentication

--- BetterHealthProject/settings.py
+++ BetterHealthProject/settings.py
@@ -121,3 +121,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # tu app personalizada
+    'patient',  # cambia si tu app tiene otro nombre
+]
+LOGIN_REDIRECT_URL = 'profile'
