437f2ed3988d5d2b61d3d28e1adb3937338041c3
7MCristy <malenaudl@gmail.com>
2025-04-17 10:31:51 +0200

    Añadido el perfil, la base de mi citas, settings y contacto

---
+++ patient/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from patient.models import PatientProfile
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True, label='Correo Electrónico')
+    name = forms.CharField(required=True, label='Nombre')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+            PatientProfile.objects.create(user=user, name=self.cleaned_data['name'])
+        return user
+
+class PatientProfileForm(forms.ModelForm):
+    class Meta:
+        model = PatientProfile
+        fields = ('name', 'profile_picture')
--- patient/models.py
+++ patient/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class PatientProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def email(self):
+        return self.user.email
