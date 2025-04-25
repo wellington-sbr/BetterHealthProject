fab21fcdb541a3f8fb6cec852a9c21695bd49776
ainhprzz <ainhoapega27@gmail.com>
2025-04-25 19:43:12 +0200

    settings.py changed for static_root

--- BetterHealthProject/settings.py
+++ BetterHealthProject/settings.py
@@ -82,7 +82,14 @@ WSGI_APPLICATION = 'BetterHealthProject.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
 }
 
 
