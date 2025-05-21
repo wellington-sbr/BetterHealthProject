6752901300cf36eaf1ac5c59ff1b3f3e62759c29
wellington-sbr <baezrwellingtons@gmail.com>
2025-05-21 22:07:32 +0200

    feat: Integrated a section to administrate services

--- betterhealth/urls.py
+++ betterhealth/urls.py
@@ -1,10 +1,7 @@
-
-
 from django.conf.urls.static import static
 from django.urls import path
 from BetterHealthProject import settings
 from . import views
-from .views import listado_facturas
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
@@ -12,7 +9,6 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
     path('home/', views.home, name='home'),
-
     path('settings/', views.settings_view, name='settings'),
     path('contact/', views.contact_view, name='contact'),
     path('programar-cita/', views.programar_cita, name='programar_cita'),
@@ -24,12 +20,16 @@ urlpatterns = [
     path('register-staff/', views.register_staff, name='register_staff'),
     path('admin-panel/', views.admin_panel, name='panel_administrativo'),
 
-    path('listado_facturas/', listado_facturas, name='listado_facturas'),
+    path('listado_facturas/', views.listado_facturas, name='listado_facturas'),
     path('panel-finanzas/', views.finances_panel, name='panel_finanzas'),
 
     path('cita/<int:cita_id>/cancelar/', views.cancelar_cita, name='cancelar_cita'),
     path('cita/<int:cita_id>/reprogramar/', views.reprogramar_cita, name='reprogramar_cita'),
-
+    path("staff/import-services/", views.import_services_view, name="import_services"),
+    path("staff/add-service/", views.add_service_view, name="add_service"),
+    path("staff/delete-service/<int:service_id>/", views.delete_service_view, name="delete_service"),
+    path("staff/export-services/", views.export_services_csv, name="export_services_csv"),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
