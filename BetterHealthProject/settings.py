04cf95abdcce250f6e25bfe61727ec2fc7c4bd1f
wellington-sbr <baezrwellingtons@gmail.com>
2025-06-17 17:09:26 +0200

    fix: Comment out default SQLite database configuration in settings.py

--- BetterHealthProject/settings.py
+++ BetterHealthProject/settings.py
@@ -89,8 +89,8 @@ WSGI_APPLICATION = 'BetterHealthProject.wsgi.application'
 
 DATABASES = {
     # 'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
     # }
 
      'default': dj_database_url.config(
