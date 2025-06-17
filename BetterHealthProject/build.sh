07a7c38a77a5d0ac36a9180b2e0b62a579c710c9
wellington-sbr <baezrwellingtons@gmail.com>
2025-06-17 16:29:37 +0200

    fix: Update build script and requirements for Poetry integration

--- BetterHealthProject/build.sh
+++ BetterHealthProject/build.sh
@@ -2,11 +2,9 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+# Install dependencies using Poetry
+poetry install --no-interaction --no-root
 
-# Convert static asset files
-python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
-python manage.py migrate
+# Run Django management commands with Poetry
+poetry run python manage.py collectstatic --no-input
+poetry run python manage.py migrate
